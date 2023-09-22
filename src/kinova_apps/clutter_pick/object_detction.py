e8545bdbeceb2d094cfeaee50a2ddceb216008f8
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-09-22 17:34:22 +0200

    updated return type to return obj perimeters as Polygons

--- src/kinova_apps/clutter_pick/object_detction.py
+++ src/kinova_apps/clutter_pick/object_detction.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2
 import cv_bridge
 from sensor_msgs.msg import Image
+from shapely.geometry import Polygon
 
 OBJECTS = {'toothbrush': 79, 'cup': 41,'fork': 42,'knife': 43,'spoon': 44,'bowl': 45}
 
@@ -36,7 +37,7 @@ class YoloDetector:
 
         return frame, predicted_boxes
     
-    def detect_segments(self, img:Image)->(np.array, Dict[str, np.array]):
+    def detect_segments(self, img:Image)->(np.array, Dict[str, np.array], List[Polygon]):
         '''
         Detect objects in an image
         
@@ -45,7 +46,8 @@ class YoloDetector:
 
         Returns:
             res_plotted (np.array): image with detected objects
-            predicted_boxes (np.array): array of bounding boxes
+            predicted_masks (np.array): array of masks
+            predicted_polygons (List[Polygon]): list of polygons
         '''
 
         # convert image to numpy array
@@ -61,16 +63,21 @@ class YoloDetector:
         res_plotted = results[0].plot(boxes=False)
 
         predicted_masks = results[0].masks.data.cpu().numpy()
+        predicted_segments = results[0].masks.segments
         predicted_classes = results[0].boxes.cls
 
         masks = {}
+        polygons = []
 
         for i, mask in enumerate(predicted_masks):
             # check if mask is empty
             if np.count_nonzero(mask) == 0:
                 continue
             masks[self.model.names[int(predicted_classes[i])]] = mask
+            # make mask into polygon
+            polygon = Polygon(predicted_segments[i])
+            polygons.append(polygon)
 
-        return res_plotted, masks
+        return res_plotted, masks, polygons
     
     
