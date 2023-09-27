91a1ec10a5f4c916b8b22a0b96562f6eadab1550
Barath19 <bk190196@gmail.com>
2023-09-27 02:01:01 +0200

    gripper masks and filtering masks

--- src/kinova_apps/clutter_pick/object_detction.py
+++ src/kinova_apps/clutter_pick/object_detction.py
@@ -6,14 +6,19 @@ import cv2
 import cv_bridge
 from sensor_msgs.msg import Image
 from shapely.geometry import Polygon
+from collections import defaultdict
+
 
 OBJECTS = {'toothbrush': 79, 'cup': 41,'fork': 42,'knife': 43,'spoon': 44,'bowl': 45}
+GRIPPER_LEFT_POINTS = np.array([[140, 720],[224, 625],[296, 625],[300, 720],[132, 720]])
+GRIPPER_RIGHT_POINTS = np.array([[996, 720],[996, 720],[996, 720],[996, 720],[996, 629],[1072, 621],[1160, 720],[988, 720]])
 
 class YoloDetector:
 
     def __init__(self, model='yolov8n.pt'):
         self.model = YOLO(model)
         self.bridge = cv_bridge.CvBridge()
+    
 
     def detect(self, img: Image):
         img = self.bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
@@ -37,7 +42,7 @@ class YoloDetector:
 
         return frame, predicted_boxes
     
-    def detect_segments(self, img:Image)->(np.array, Dict[str, np.array], List[Polygon]):
+    def detect_segments(self, img:Image)->(np.array, List[str] ,List[np.array], List[Polygon]):
         '''
         Detect objects in an image
         
@@ -56,28 +61,54 @@ class YoloDetector:
         # convert image to RGB
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+        # add gripper masks
+        
+        cv2.fillPoly(img, pts=[GRIPPER_LEFT_POINTS, GRIPPER_RIGHT_POINTS], color=(0, 255, 0))
+
         # detect objects
         results = self.model.predict(source=img, conf=0.6, iou=0.45, retina_masks=True)
         
         # plot results
-        res_plotted = results[0].plot(boxes=False)
+        res_plotted = results[0].plot(boxes=True, labels=True)
 
         predicted_masks = results[0].masks.data.cpu().numpy()
         predicted_segments = results[0].masks.segments
         predicted_classes = results[0].boxes.cls
 
-        masks = {}
+
         polygons = []
+        classes = []
+        class_masks = []
 
         for i, mask in enumerate(predicted_masks):
-            # check if mask is empty
-            if np.count_nonzero(mask) == 0:
-                continue
-            masks[self.model.names[int(predicted_classes[i])]] = mask
+            # check if mask is 
+
+            classes.append(self.model.names[int(predicted_classes[i])])
+            class_masks.append(mask)
             # make mask into polygon
             polygon = Polygon(predicted_segments[i])
             polygons.append(polygon)
 
-        return res_plotted, masks, polygons
+        return res_plotted, classes, class_masks, polygons
     
     
+
+""" if __name__ == '__main__':
+
+    # test detector
+
+    img = cv2.imread('/home/bk/competition/white/white022.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    detector = YoloDetector(model='/home/bk/competition/kinova_apps/models/clutter_picking/best_all_object.pt')
+    res_plotted, names,masks, polygons = detector.detect_segments(img)
+
+    print(names)
+
+    for i, mask in enumerate(masks):
+        cv2.imshow(names[i], mask)
+        cv2.waitKey(0)
+    cv2.imshow('test', res_plotted)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+ """
